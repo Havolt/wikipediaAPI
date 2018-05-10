308d69dd03f01e69b7103afad6fc5124353ce131
Mark Fitzpatrick <markfitz815@gmail.com>
2018-05-10 19:45:23 +0100

    ordered data from callback function into readable links

--- codeTrain/wiki.js
+++ codeTrain/wiki.js
@@ -7,20 +7,33 @@ function setup(){
 }
 
 function searchData(data){
-    console.log(data);
+    console.log(data)
 
-    document.querySelector('.searchResult').innerHTML = data;
+    document.querySelector('.searchResult').innerHTML = '';
+
+    for(let i = 0; i < data[1].length; i++){
+        let newEl = document.createElement('h2');
+        newEl.innerHTML = data[1][i];
+        newEl.classList.add('searchLink');
+        newEl.href = data[3][i];
+        newEl.onclick = function(){
+            window.open(data[3][i]);
+        }
+        document.querySelector('.searchResult').appendChild(newEl)
+
+        let newDesc = document.createElement('p');
+        newDesc.innerHTML = data[2][i];
+        newDesc.classList.add('searchDesc');
+        document.querySelector('.searchResult').appendChild(newDesc)
+    }
 }
 
 
 function goWiki(){
     let term = document.querySelector('#userinput').value;
-
     let s = document.createElement('script');
     s.src= 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search='+term+'&callback=searchData';
     document.body.appendChild(s);
-    
-    
 }
 
 
